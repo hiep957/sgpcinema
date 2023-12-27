a8d68007fd58922a4c71d20729a03b78cf6ded9d
hicharmie1 <hicharmie1@gmail.com>
2023-12-27 10:58:52 +0700

    main

--- src/components/HomeSlider/HomeSlider.tsx
+++ src/components/HomeSlider/HomeSlider.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState,useEffect } from 'react';
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
 
@@ -9,20 +9,41 @@ import 'swiper/css/pagination';
 
 import { Navigation, Pagination, Mousewheel, Keyboard } from 'swiper/modules';
 import Image from 'next/image';
-let width = 700;
-let height = 800;
-if (typeof window !== 'undefined') {
-    // Truy cập window chỉ khi ở môi trường client-side
-    width = window.innerWidth;
-    height = window.innerHeight;
-} else {
-    // Xử lý khi không có window (môi trường server-side)
-    // Đây là nơi bạn có thể thiết lập giá trị mặc định hoặc xử lý khác
-    // Ví dụ:
-    width = 800; // Giá trị mặc định cho width khi không có window
-    height = 600; // Giá trị mặc định cho height khi không có window
-}
+//  const width = window.innerWidth;
+//     const height = window.innerHeight;
+// let width = 700;
+// let height = 800;
+// if (typeof window !== 'undefined') {
+//     // Truy cập window chỉ khi ở môi trường client-side
+    
+// } else {
+//     // Xử lý khi không có window (môi trường server-side)
+//     // Đây là nơi bạn có thể thiết lập giá trị mặc định hoặc xử lý khác
+//     // Ví dụ:
+//     width = 800; // Giá trị mặc định cho width khi không có window
+//     height = 600; // Giá trị mặc định cho height khi không có window
+// }
 const HomeSlider = () => {
+    const [width, setWidth] = useState(window.innerWidth);
+    const [height, setHeight] = useState(window.innerHeight);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setWidth(window.innerWidth);
+            setHeight(window.innerHeight);
+        };
+
+        // Set initial dimensions
+        handleResize();
+
+        // Listen for window resize events
+        window.addEventListener('resize', handleResize);
+
+        // Cleanup event listener on component unmount
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
 
     const [banners, setBanners] = useState([
         {
